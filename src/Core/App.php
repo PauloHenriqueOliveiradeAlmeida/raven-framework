484acaf5bd717ad6a74971b5d8cc6ed7c263a514
PauloHOAlmeida <paulo.almeida@epiousion.com.br>
2024-10-22 20:25:00 -0300

    :bug: fix: resolve warnings

--- src/Core/App.php
+++ src/Core/App.php
@@ -9,25 +9,25 @@ set_exception_handler(ExceptionHandler::throwException(...));
 
 class App
 {
-	public function __construct(AppConfig $appConfig)
-	{
-		$envFile = file(__DIR__ . '/../../../../../.env');
+  public function __construct(AppConfig $appConfig)
+  {
+    $envFile = file(__DIR__ . '/../../../../../.env');
 
-		if ($envFile) {
-			foreach ($envFile as $line) {
-				if (trim($line)[0] === '#' || trim($line) === '') continue;
+    if ($envFile) {
+      foreach ($envFile as $line) {
+        if (trim($line) === '' || trim($line)[0] === '#') continue;
 
-				$line = trim(str_replace('\\n', '', $line));
-				putenv($line);
-			}
-		}
+        $line = trim(str_replace('\\n', '', $line));
+        putenv($line);
+      }
+    }
 
-		$routeHandler = new RouteHandler($appConfig);
-		$routeHandler->manageRoute();
-	}
+    $routeHandler = new RouteHandler($appConfig);
+    $routeHandler->manageRoute();
+  }
 
-	public static function bootstrap(AppConfig $appConfig)
-	{
-		return new static($appConfig);
-	}
+  public static function bootstrap(AppConfig $appConfig)
+  {
+    return new static($appConfig);
+  }
 }
--- src/Falcon/Http/Exceptions/NotFoundException.php
+++ src/Falcon/Http/Exceptions/NotFoundException.php
@@ -6,9 +6,9 @@ use Raven\Falcon\Http\StatusCode;
 
 class NotFoundException extends \Exception
 {
-	public function __construct(string $message = null, $code = StatusCode::NOT_FOUND, \Exception $previous = null)
-	{
-		parent::__construct($message ?? 'Not Found', $code->value, $previous);
-		header('Content-Type: application/json');
-	}
+  public function __construct(string $message = null, $code = StatusCode::NOT_FOUND, \Exception $previous = null)
+  {
+    parent::__construct($message ?? 'Not Found', $code->value, $previous);
+    header('Content-Type: application/json');
+  }
 }
